d0e5dbc3f468a372036d00ad5d4d2bf19869e7aa
김지섭 <jiseob812@gmail.com>
2021-05-30 18:16:01 +0900

    CodingKey 포스팅 소스코드 옮겨옴

---
+++ BlogSourceFile/MainViewController.swift
@@ -0,0 +1,61 @@
+//
+//  MainViewController.swift
+//  BlogSourceFile
+//
+//  Created by 김지섭 on 2021/05/30.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    enum CellInfo: String, CaseIterable {
+        case codingKey = "CodingKey 포스팅"
+        
+        
+        var segueID: String {
+            switch self {
+            case .codingKey: return "CodingKey"
+            }
+        }
+        
+        var cellID: String { return "Cell" }
+    }
+    
+    @IBOutlet weak var tableView: UITableView! {
+        didSet {
+            tableView.delegate = self
+            tableView.dataSource = self
+        }
+    }
+    
+    var cellInfos: [CellInfo] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        cellInfos = CellInfo.allCases
+    }
+}
+
+extension MainViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cellInfos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let info = cellInfos[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: info.cellID, for: indexPath)
+        let label = cell.viewWithTag(1) as? UILabel
+        let title = info.rawValue
+        
+        label?.text = title
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let info = cellInfos[indexPath.row]
+        performSegue(withIdentifier: info.segueID, sender: nil)
+    }
+}
---
+++ BlogSourceFile/Postings/CodingKeyWithOtherTypeVC.swift
@@ -0,0 +1,107 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by kimjiseob on 25/07/2019.
+//  Copyright © 2019 kimjiseob. All rights reserved.
+//
+
+import UIKit
+
+class CodingKeyWithOtherTypeVC: UIViewController {
+
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        testDiffType()
+        
+    }
+    
+    
+    func testCondingKey() {
+        // 1. 통신 결과값
+        let response = DummyJsonLoader.load(api: .postingAPI)!
+        
+        do {
+            // 2. Codable을 이용한 모델 얻기
+            let value = try JSONDecoder().decode(Model2.self, from: response)
+            // 3. 결과 출력
+            let result = """
+                userNm = \(value.userName),
+                sequenceNo = \(value.sequenceNo),
+                orderNo = \(value.orderNo),
+                address = \(value.address),
+                """
+                
+            print(result)
+            resultLabel.text = result
+            
+            
+        } catch (let error) {
+            // 4. 오류 출력
+            print(error)
+        }
+    }
+    
+    struct Model: Codable {
+        let userName: String
+        let isHidden: Bool
+        
+        enum CodingKeys: String, CodingKey {
+            case userName
+            case isHidden
+        }
+        
+        init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            
+            self.userName = try container.decode(String.self, forKey: .userName)
+            self.isHidden = try container.decode(String.self, forKey: .isHidden) == "Y"
+        }
+    }
+    
+    // 테스트
+    func testDiffType() {
+        // 1. 통신 결과값
+        let response = DummyJsonLoader.load(api: .dataType)!
+        
+        do {
+            // 2. Codable을 이용한 모델 얻기
+            let value = try JSONDecoder().decode(Model.self, from: response)
+            // 3. 결과 출력
+            let result = """
+                userNm = \(value.userName),
+                isHidden = \(value.isHidden)
+                """
+            
+            print(result)
+            resultLabel.text = result
+            
+        } catch (let error) {
+            // 4. 오류 출력
+            print(error)
+        }
+    }
+}
+
+
+
+
+
+struct Model2: Codable {
+    let userName: String
+    let sequenceNo: Int
+    let orderNo: Int
+    let address: String
+    
+    enum CodingKeys: String, CodingKey {
+        case userName = "usrNm"
+        case sequenceNo = "seqNo"
+        case orderNo = "ordNo"
+        case address = "addr"
+    }
+}
+
+
