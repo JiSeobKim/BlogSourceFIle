27ab93070b1bf0ca5483307ddc2123fca1c606ad
JiseobKim <jiseob812@gmail.com>
2021-06-27 22:51:06 +0900

    Property Wrapper Generic 다시 정의중

--- BlogSourceFile/PropertyWrapper/PropertyWrapper_Generic.swift
+++ BlogSourceFile/PropertyWrapper/PropertyWrapper_Generic.swift
@@ -1,61 +1,11 @@
 //
-//  CodableWrapper.swift
+//  PropertyWrapper_High.swift
 //  BlogSourceFile
 //
-//  Created by 김지섭 on 2021/06/12.
+//  Created by 김지섭 on 2021/06/27.
 //
 
 import Foundation
-
-
-@propertyWrapper
-struct StringBoolConverter {
-    let wrappedValue: Bool
-}
-
-extension StringBoolConverter: Codable {
-    init(from decoder: Decoder) throws {
-        let container = try decoder.singleValueContainer()
-        let stringBool = try? container.decode(String.self)
-        wrappedValue = stringBool == "Y"
-    }
-}
-
-
-@propertyWrapper
-struct ZeroDefault<T: Numeric> {
-    var wrappedValue: T
-    
-    
-}
-
-
-extension ZeroDefault: Codable where T: Codable  {
-    init() {
-        wrappedValue = 0
-    }
-    
-    init(from decoder: Decoder) throws {
-        let container = try decoder.singleValueContainer()
-        
-        self.wrappedValue = try container.decode(T.self)
-        
-    }
-}
-
-
-extension KeyedDecodingContainer {
-    func decode(_ type: ZeroDefault<Int>.Type,
-                forKey key: Key) throws -> ZeroDefault<Int> {
-        try decodeIfPresent(type, forKey: key) ?? .init()
-    }
-    
-    func decode(_ type: ZeroDefault<Double>.Type,
-                forKey key: Key) throws -> ZeroDefault<Double> {
-        try decodeIfPresent(type, forKey: key) ?? .init()
-    }
-}
-
 
 // MARK: - High Level
 protocol DecodableDefaultSource {
